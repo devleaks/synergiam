2da84f61c321947ab24c8cbed4a6631d05278f4a
Pierre M <pierre@New-iMac.local>
2015-03-18 13:58:31 +0100

    Initial commit

---
+++ js/synergiam.js
@@ -0,0 +1,27 @@
+jQuery(function($) {'use strict';
+
+	$('a.profile').siblings("span").toggle();
+
+	$('a.profile').click(function() {
+		$(this).siblings("span").toggle();
+		$(this).toggle();
+	});
+
+	$('span.profile').click(function() {
+		$(this).toggle();
+		$(this).siblings("a").toggle();
+	});
+
+// infinitescroll() is called on the element that surrounds 
+// the items you will be loading more of
+	if(jQuery.infinitescroll)
+		$('#content').infinitescroll({
+			navSelector  : "div.navigation",            
+			               // selector for the paged navigation (it will be hidden)
+			nextSelector : "div.navigation a:first",    
+			               // selector for the NEXT link (to page 2)
+			itemSelector : "#content div.row"          
+			               // selector for all items you'll retrieve
+		});
+	
+});
